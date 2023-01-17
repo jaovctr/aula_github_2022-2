e4e7511425c9480799eb998118493d4d5f52123e
Maria Clara <mariacoelho.alves02@gmail.com>
2023-01-16 22:08:57 -0300

    Issue #9 resolvida.

--- Cliente.java
+++ Cliente.java
@@ -3,12 +3,39 @@ public class Cliente {
     private String nome;
     private String cpf;
 
-    public String getCliente(){
-        return "id: "+id+"\nNome: "+nome+"\nCPF: "+cpf;
-    }
-    public void setCliente(int id, String nome, String cpf){
+    public Cliente(int id, String nome, String cpf){
         this.id = id;
         this.nome = nome;
         this.cpf = cpf;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String toString(){
+        return "id: "+id+"\nNome: "+nome+"\nCPF: "+cpf;
+    }
+
+
 }
