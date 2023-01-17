a172130d960a777283949aca5eeca2c80f8c2ead
Caio Feitosa <franciscocaiof@gmail.com>
2023-01-16 22:39:12 -0300

    Cadastro de novos clientes implementado #11

--- Cliente.java
+++ Cliente.java
@@ -2,10 +2,17 @@ public class Cliente {
     private int id;
     private String nome;
     private String cpf;
-
+    
+    public Cliente(int id, String nome, String cpf){
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    
     public String getCliente(){
         return "id: "+id+"\nNome: "+nome+"\nCPF: "+cpf;
     }
+    
     public void setCliente(int id, String nome, String cpf){
         this.id = id;
         this.nome = nome;
--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
-package aula;
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+	
+	private static String ler_terminal(String texto) {
+		System.out.println(texto);
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+
+		return str;
+	}
 	
 	public static void menuConta() {
 		Menu mainMenu =  new Menu("Menu Conta", Arrays.asList("Depositar", "Sacar", "Saldo", "Sair"));
@@ -25,6 +33,8 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
+		ArrayList<Cliente> clientes = new ArrayList();
+		
 		Menu mainMenu =  new Menu("Menu Principal",
 				Arrays.asList(
 						"Cadastrar Cliente",
@@ -33,12 +43,16 @@ public class Main {
 						"Sair"
 				));
 		int op;
+		int id = 0;
 		
 		do {
 			op = mainMenu.getSelection();
 			
 			switch(op){
 				case 1:
+					String nome = ler_terminal("Nome: ");
+					String cpf = ler_terminal("CPF: ");
+					clientes.add(new Cliente(++id, nome, cpf));
 					break;
 				case 2:
 					break;
